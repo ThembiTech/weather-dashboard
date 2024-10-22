d07dcefa0c13ef8d0e20be50ff15262c17fb08de
ThembiTech <thembisaskosana785@gmail.com>
2024-10-22 22:30:18 +0200

    Updated index.html

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function search(event) {
   
   let city = searchInputElement.value;
 
-  let apiKey=apiKey;
+ // let apiKey=apiKey;
   let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`; 
 
   axios.get(apiUrl).then(displayTemperature);
